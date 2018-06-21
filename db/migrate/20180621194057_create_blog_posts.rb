6c441f8ec70fae914821288cb3dc4e4b8df9f3aa
IanLockwood <mrianlockwood@gmail.com>
2018-06-21 15:45:25 -0400

    Actually create the blog posts table

--- db/migrate/20180621194057_create_blog_posts.rb
+++ db/migrate/20180621194057_create_blog_posts.rb
@@ -2,6 +2,11 @@ class CreateBlogPosts < ActiveRecord::Migration[5.0]
   def change
     create_table :blog_posts do |t|
 
+      t.string :title
+      t.string :author
+      t.text :tags, array: true, default: []
+      t.text :content
+
       t.timestamps
     end
   end
